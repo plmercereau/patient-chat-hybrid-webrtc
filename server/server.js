c474eddd784721dafce19639c52a9b97c9f48f5c
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2020-05-21 15:21:26 +0200

    feat: self-signed certificates

--- server/server.js
+++ server/server.js
@@ -1,16 +1,36 @@
 const express = require('express')
 const path = require('path')
 const { ExpressPeerServer } = require('peer')
+const https = require('https')
+const selfsigned = require('selfsigned')
 const log = require('./log')
 
 module.exports = function(port = 3000) {
+  var attrs = [{ name: 'commonName', value: 'pocophonef1-pocophon' }]
+  var pems = selfsigned.generate(attrs, { days: 365 })
+  console.log(pems)
+
   const app = express()
   app.use(express.static(path.join(__dirname, 'client')))
   log('Static client middleware loaded.')
 
-  const server = app.listen(port, () => {
-    log(`App listening at http://0.0.0.0:${port}`)
-  })
+  // we will pass our 'app' to 'https' server
+  const server = https
+    .createServer(
+      {
+        key: perms.private,
+        cert: perms.cert
+        // passphrase: 'YOUR PASSPHRASE HERE'
+      },
+      app
+    )
+    .listen(port, () => {
+      log(`App listening at http://0.0.0.0:${port}`)
+    })
+
+  //   const server = app.listen(port, () => {
+  //     log(`App listening at http://0.0.0.0:${port}`)
+  //   })
 
   const peerServer = ExpressPeerServer(server, {
     debug: true,
