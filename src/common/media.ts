d0e07ed07dd9bb01b0cdb576052153cfdc268aee
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2020-05-28 13:49:57 +0200

    refactor: code cleanup

--- src/common/media.ts
+++ src/common/media.ts
@@ -2,7 +2,7 @@
 import { setLocalStream, getLocalStream } from 'src/store/peer'
 
 // ? Move elsewhere?
-const setNavLocalStream = async () => {
+export const startCamera = async () => {
   if (navigator.mediaDevices) {
     const media = await navigator.mediaDevices.getUserMedia({
       audio: true,
@@ -19,30 +19,6 @@ const setNavLocalStream = async () => {
   } else {
     throw Error('NO MEDIA DEVICES??')
   }
-}
-
-// ? Move elsewhere?
-export const startCamera = async () => {
-  return await setNavLocalStream()
-  // TODO make the plugin permission work
-  //   if (permissionsPlugin) {
-  //     console.log('PERMISSION PLUGIN')
-  //     const perms = [
-  //       permissionsPlugin.MODIFY_AUDIO_SETTINGS,
-  //       permissionsPlugin.RECORD_AUDIO,
-  //       permissionsPlugin.CAPTURE_AUDIO_OUTPUT,
-  //       permissionsPlugin.CAMERA
-  //     ]
-
-  //     permissionsPlugin.requestPermission(
-  //       perms,
-  //       () => setNavLocalStream(),
-  //       () => console.log('FUNCKING NIGHTMARE')
-  //     )
-  //   } else {
-  //     console.log('NO PERMISSION PLUGIN')
-  //     setNavLocalStream()
-  //   }
 }
 
 export const stopCamera = () => {
